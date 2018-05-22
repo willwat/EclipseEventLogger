9fcddf7e9433e05b8942235f3b1da576e29cbae4
watsonwd <watsonwd@mail.uc.edu>
2018-05-22 12:48:43 -0400

    Restructured startup adding Main

---
+++ EclipseErrorRecorder/src/main/Main.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package main;
+
+import org.eclipse.ui.IStartup;
+
+/**
+ * @author William
+ *
+ */
+public class Main implements IStartup {
+
+	@Override
+	public void earlyStartup() {
+		
+	}
+
+}
