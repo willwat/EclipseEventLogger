4debdf9847c82d5faaf171290716893923485605
watsonwd <watsonwd@mail.uc.edu>
2018-05-29 10:47:30 -0400

    Stopped program from grabbing duplicate errors from typing

--- EclipseErrorRecorder/src/handlers/AnnotationModelChangedHandler.java
+++ EclipseErrorRecorder/src/handlers/AnnotationModelChangedHandler.java
@@ -1,5 +1,9 @@
 package handlers;
 
+
+import java.util.ArrayList;
+
+import org.eclipse.jface.text.Position;
 import org.eclipse.jface.text.source.Annotation;
 import org.eclipse.jface.text.source.AnnotationModelEvent;
 import org.eclipse.jface.text.source.IAnnotationModel;
@@ -8,22 +12,33 @@ import org.eclipse.jface.text.source.IAnnotationModelListenerExtension;
 
 public class AnnotationModelChangedHandler implements IAnnotationModelListenerExtension, IAnnotationModelListener {
 
+	ArrayList<ArrayList<Object>> checkedAnnotationInfo;
+	
+	public AnnotationModelChangedHandler() {
+		checkedAnnotationInfo = new ArrayList<ArrayList<Object>>();
+	}
+	
 	@Override
 	public void modelChanged(AnnotationModelEvent event) {
 		Annotation[] addedAnnotations = event.getAddedAnnotations();
-
+		
 		for(Annotation ann : addedAnnotations) {
-			if(ann.getType().equals("org.eclipse.jdt.ui.error") && !ann.getText().contains("chkd")) {
-				System.out.println(ann.getText() + " Type: " + ann.getType());
-				ann.setText(ann.getText() + "chkd");
+			Position annPos = event.getAnnotationModel().getPosition(ann);
+			ArrayList<Object> annotationInfo = new ArrayList<Object>();
+			annotationInfo.add(ann.getText());
+			annotationInfo.add(annPos.getOffset());
+			
+			if(ann.getType().equals("org.eclipse.jdt.ui.error") && !checkedAnnotationInfo.contains(annotationInfo)) {
+				System.out.println("Annotation Info:\t" + "Text: " + ann.getText());
+				System.out.println("Annotation Position Info:\t" + "Length: " + annPos.getLength() + "\t" + "Offset: " + annPos.getOffset());
+				System.out.println();
+				checkedAnnotationInfo.add(annotationInfo);
 			}
 		}
-		System.out.println();		
 	}
 
 	@Override
 	public void modelChanged(IAnnotationModel model) {
-		// TODO Auto-generated method stub
 		
 	}
 
