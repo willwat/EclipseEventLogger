bacc5fa903dbadbe91469d5087f36237e6906e46
watsonwd <watsonwd@mail.uc.edu>
2018-06-13 13:36:15 -0400

    Added start to filtering some syntax errors.

--- EclipseErrorRecorder/src/handlers/AnnotationModelChangedHandler.java
+++ EclipseErrorRecorder/src/handlers/AnnotationModelChangedHandler.java
@@ -18,6 +18,7 @@ public class AnnotationModelChangedHandler implements IAnnotationModelListenerEx
 
 	ArrayList<ArrayList<Object>> checkedAnnotationInfo;
 	private final int syntaxErrorTypeID = 2;
+	private long timeOfLastModelChangeInMilis = System.currentTimeMillis() - 4000;
 	
 	public AnnotationModelChangedHandler() {
 		checkedAnnotationInfo = new ArrayList<ArrayList<Object>>();
@@ -26,21 +27,24 @@ public class AnnotationModelChangedHandler implements IAnnotationModelListenerEx
 	@Override
 	public void modelChanged(AnnotationModelEvent event) {
 		Annotation[] addedAnnotations = event.getAddedAnnotations();
-		
-		for(Annotation ann : addedAnnotations) {
-			Position annPos = event.getAnnotationModel().getPosition(ann);
-			ArrayList<Object> annotationInfo = new ArrayList<Object>();
-			annotationInfo.add(ann.getText());
-			annotationInfo.add(annPos.getOffset());
-			
-			if(ann.getType().equals("org.eclipse.jdt.ui.error") && !checkedAnnotationInfo.contains(annotationInfo)) {
-				try {
-					DBUtils.addRecordToDB(GeneralUtils.getUserMACAddress(), ann.getText(), syntaxErrorTypeID);
-				} catch (SQLException e) {
-					return;
+		if(System.currentTimeMillis() - timeOfLastModelChangeInMilis >= 2000) {
+			for(Annotation ann : addedAnnotations) {
+				Position annPos = event.getAnnotationModel().getPosition(ann);
+				ArrayList<Object> annotationInfo = new ArrayList<Object>();
+				annotationInfo.add(ann.getText());
+				annotationInfo.add(annPos.getOffset());
+				
+				if(ann.getType().equals("org.eclipse.jdt.ui.error") && !checkedAnnotationInfo.contains(annotationInfo)) {
+					try {
+						DBUtils.addRecordToDB(GeneralUtils.getUserMACAddress(), ann.getText(), syntaxErrorTypeID);
+					} catch (SQLException e) {
+						return;
+					}
 				}
 			}
 		}
+		
+		timeOfLastModelChangeInMilis = System.currentTimeMillis();
 	}
 
 	@Override
